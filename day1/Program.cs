98b4b6afa1b7cf49a2d2aeeec94a1e54598962d2
Alex Dragomir <alex.dragomir@caphyon.com>
2025-12-02 14:34:52 +0200

    days 1 and 2

---
+++ day1/Program.cs
@@ -0,0 +1,20 @@
+﻿var moves = File.ReadAllLines("in.txt").Select(l => int.Parse(l.Substring(1)) * (l[0] == 'L' ? -1 : 1));
+
+int currentPos = 50;
+int counter = 0;
+int counter2 = 0;
+foreach (var move in moves)
+{
+  int nextPos = currentPos + move;
+  if(nextPos % 100 == 0)
+    counter++;
+
+  if (nextPos <= 0 && currentPos != 0)
+    counter2++;
+
+  counter2 += Math.Abs(nextPos / 100);
+  currentPos = (100 + nextPos % 100) % 100;
+}
+
+Console.WriteLine(counter);
+Console.WriteLine(counter2);
---
+++ day2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+var input = File.ReadAllText("in.txt");
+
+var ids = input.Split(",").Select(tokens => tokens.Split("-").Select(id => long.Parse(id)).ToList());
+
+long sum = 0;
+long sum2 = 0;
+foreach (var idPair in ids)
+{
+  for (long i = idPair[0]; i <= idPair[1]; i++)
+  {
+    if (isInvalid(i.ToString()))
+      sum += i;
+
+    if (isInvalid2(i.ToString()))
+      sum2 += i;
+  }
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(sum2);
+
+bool isInvalid(string id)
+{
+  return id.Substring(0, id.Length / 2) == id.Substring(id.Length / 2);
+}
+
+bool isInvalid2(string id)
+{
+  if(id.Length == 1)
+    return false;
+
+  if(id.All(c=> c == id[0]))
+    return true;
+
+  for (int i = 2; i <= id.Length/2; i++)
+  {
+    if(id.Length % i != 0)
+      continue;
+
+    var chunks = Split(id, i);
+    if(chunks.All(c => c == chunks.First()))
+      return true;
+  }
+
+  return false;
+}
+
+IEnumerable<string> Split(string str, int chunkSize)
+{
+  return Enumerable.Range(0, str.Length / chunkSize)
+      .Select(i => str.Substring(i * chunkSize, chunkSize));
+}
